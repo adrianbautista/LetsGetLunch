e21fbd7b005ac96e500cc7ab4d97cdd36ded6ea4
Adrian Bautista <adrianbautista8@gmail.com>
2013-12-18 01:07:36 -0500

    Create basic lunch_date form

--- app/controllers/lunch_dates_controller.rb
+++ app/controllers/lunch_dates_controller.rb
@@ -2,5 +2,10 @@ class LunchDatesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @lunch_dates = current_user.lunch_dates
+  end
+
+  def new
+    @lunch_date = LunchDate.new
   end
 end
